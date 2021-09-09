911c90ed865a06c756e786fc9c23327571c73681
pang623 <pang623j@gmail.com>
2021-09-09 16:08:16 +0800

    Test github actions

--- test/test_ExpressionParser.c
+++ test/test_ExpressionParser.c
@@ -51,7 +51,7 @@ void test_expression_given_3_plus_2_times_4_expect_correctly_parsed(void) {
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -82,7 +82,7 @@ void test_expression_given_NEG3_MINUS_2_times_NEG4_expect_correctly_parsed(void)
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -109,7 +109,7 @@ void test_expression_given_3_postincrement_plus_2_expect_correctly_parsed(void) 
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -142,7 +142,7 @@ void test_expression_given_mix_of_prefixes_and_suffixes_expect_correctly_parsed(
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -169,7 +169,7 @@ void test_expression_given_plus_plus_but_not_adjacent_expect_not_parsed_as_inc(v
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -199,7 +199,7 @@ void test_expression_given_minus_minus_but_not_adjacent_expect_not_parsed_as_dec
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -233,7 +233,7 @@ void test_expression_given_expression_with_assignment_operator_expect_ast_create
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -261,7 +261,7 @@ void test_expression_given_expression_with_array_expect_ast_created_correctly(vo
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -278,12 +278,12 @@ void test_expression_with_single_number_expect_ast_created_correctly(void) {
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_FAIL_MESSAGE("System Error: Don't expect any exception to be thrown!");
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
 }
-/*
+
 void test_expression_given_3_and_2_expect_ERR_SYNTAX_is_thrown(void) {
   Symbol *symbol = NULL;
   Tokenizer *tokenizer = createTokenizer("3 2");
@@ -294,12 +294,12 @@ void test_expression_given_3_and_2_expect_ERR_SYNTAX_is_thrown(void) {
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
 }
-
+/*
 void test_expression_given_expression_with_missing_operator_expect_ERR_SYNTAX_is_thrown(void) {
   Symbol *symbol = NULL;
   //parsed as (a++)b, missing operator before b
@@ -311,7 +311,7 @@ void test_expression_given_expression_with_missing_operator_expect_ERR_SYNTAX_is
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -328,7 +328,7 @@ void test_expression_given_binary_only_operator_used_as_unary_expect_error_synta
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -345,7 +345,7 @@ void test_expression_given_unary_only_operator_used_as_binary_expect_error_synta
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -362,7 +362,7 @@ void test_expression_given_missing_operand_for_binary_operator_expect_error_miss
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_MISSING_OPERAND, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -379,7 +379,7 @@ void test_expression_given_missing_operand_for_unary_operator_expect_error_missi
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_MISSING_OPERAND, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -396,7 +396,7 @@ void test_expression_given_open_parent_used_as_infix_expect_error_syntax_is_thro
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -413,7 +413,7 @@ void test_expression_given_missing_closing_parent_expect_error_wrong_symbol_is_t
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_WRONG_SYMBOL, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -430,7 +430,7 @@ void test_expression_given_a_shorthand_left_shift_but_used_as_prefix_expect_erro
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -447,7 +447,7 @@ void test_expression_given_a_shorthand_right_shift_but_right_operand_is_not_a_nu
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_SYNTAX, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -464,7 +464,7 @@ void test_expression_given_an_EOL_but_with_no_operands_in_front_of_it_expect_err
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_MISSING_OPERAND, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -481,7 +481,7 @@ void test_expression_given_an_expression_but_with_unknown_symbol_expect_error_in
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_INVALID_SYMBOL, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
@@ -498,7 +498,7 @@ void test_expression_given_an_expression_but_unsupported_symbol_expect_error_inv
   } Catch(e){
     dumpTokenErrorMessage(e, __LINE__);
     TEST_ASSERT_EQUAL(ERR_INVALID_SYMBOL, e->errorCode);
-    free(e);
+    freeException(e);
   }
   freeSymbol(symbol);
   freeSymbolParser(symbolParser);
